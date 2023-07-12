73407ac2b1114ce9f59f5827faf36c2eee1026d8
Hansjason97 <hansadiogo@gmail.com>
2023-07-12 07:04:59 +0100

    styling menu items

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 function MenuItem({title, imageUrl, size}) {
 
-  let isLarge = 'h-[240px]'
+  let isLarge = ''
   if(size != null) isLarge = 'h-[480px]'
 
   return (
-    <div className={`menu-item ${isLarge} `} 
-    style={{backgroundImage: `url(${imageUrl})`}}>
+    <div className={`group menu-item ${isLarge}`} >
+      <div className='background-image' 
+      style={{backgroundImage: `url(${imageUrl})`}}/>
                 <div className='content'>
-                    <h1 className='title'>{title}</h1>
+                    <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
-            </div>
+    </div>
   )
 }
 
